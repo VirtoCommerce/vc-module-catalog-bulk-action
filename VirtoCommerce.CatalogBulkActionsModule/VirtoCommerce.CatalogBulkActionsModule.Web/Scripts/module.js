0ba6b1f0642061f671770a4bf317483a24842079
Timofey <tburashov@gmail.com>
2019-10-07 11:27:47 +0700

    VP-689: the opening of the blade on click

--- VirtoCommerce.CatalogBulkActionsModule/VirtoCommerce.CatalogBulkActionsModule.Web/Scripts/module.js
+++ VirtoCommerce.CatalogBulkActionsModule/VirtoCommerce.CatalogBulkActionsModule.Web/Scripts/module.js
@@ -26,8 +26,23 @@ angular.module(moduleName, [])
                 });
         }
     ])
-    .run(['$rootScope', 'platformWebApp.mainMenuService', 'platformWebApp.widgetService', '$state', 'platformWebApp.toolbarService',
-        function ($rootScope, mainMenuService, widgetService, $state, toolbarService) {
+    .run([
+        '$rootScope',
+        'platformWebApp.mainMenuService',
+        'platformWebApp.widgetService',
+        '$state', 'platformWebApp.toolbarService',
+        'platformWebApp.bladeUtils',
+        'virtoCommerce.catalogModule.catalogBulkActionService',
+        'platformWebApp.bladeNavigationService',
+        function ($rootScope,
+            mainMenuService,
+            widgetService,
+            $state,
+            toolbarService,
+            bladeUtils,
+            catalogBulkActionService,
+            bladeNavigationService) {
+
             function isItemsChecked(blade) {
                 var gridApi = blade.$scope.gridApi;
                 return gridApi && _.any(gridApi.selection.getSelectedRows());
@@ -44,13 +59,66 @@ angular.module(moduleName, [])
             };
             mainMenuService.addMenuItem(menuItem);
 
+            catalogBulkActionService.register({
+                name: 'ChangeCategoryBulkUpdateAction',
+                controller: 'virtoCommerce.catalogModule.changeCategoryActionStepsController',
+                template: 'Modules/$(VirtoCommerce.CatalogBulkActionsModule)/Scripts/blades/action-change-category.tpl.html',
+            });
+
+            catalogBulkActionService.register({
+                name: 'UpdatePropertiesBulkUpdateAction',
+                controller: 'virtoCommerce.catalogModule.editPropertiesActionController',
+                template: 'Modules/$(VirtoCommerce.CatalogBulkActionsModule)/Scripts/blades/action-edit-properties.tpl.html',
+            });
+
+
             toolbarService.register({
                 name: "Bulk Actions", icon: 'fa fa-cubes',
                 executeMethod: function (blade) {
-                    //console.log('test: ' + this.name + this.icon + blade);
+                    var gridApi = blade.$scope.gridApi;
+
+                    var newBlade = {
+                        id: 'catalogBulkActions',
+                        title: 'catalogBulkActions.blades.exporter-list.title',
+                        subtitle: 'catalogBulkActions.blades.exporter-list.subtitle',
+                        catalog: blade.catalog,
+                        controller: 'virtoCommerce.catalogModule.actionListController',
+                        template: 'Modules/$(virtoCommerce.catalogBulkActionsModule)/Scripts/blades/action-list.tpl.html',
+                        selectedProducts: _.filter(gridApi.selection.getSelectedRows(), function (x) { return x.type === 'product' }),
+                        selectedCategories: _.filter(gridApi.selection.getSelectedRows(), function (x) { return x.type === 'category' }),
+                        dataQuery: {}
+                    };
+
+                    if (gridApi.selection.getSelectAllState()) {
+                        var searchData = {
+                            searchCriteria: {
+                                catalogId: blade.catalogId,
+                                categoryId: blade.categoryId,
+                                keyword: filter.keyword ? filter.keyword : undefined,
+                                searchInVariations: filter.searchInVariations ? filter.searchInVariations : false,
+                                responseGroup: 'withCategories, withProducts',
+                                //sort: uiGridHelper.getSortExpression($scope),
+                                //skip: ($scope.pageSettings.currentPage - 1) * $scope.pageSettings.itemsPerPageCount,
+                                //take: $scope.pageSettings.itemsPerPageCount,
+                                skip: undefined,
+                                take: undefined
+                            }
+                        };
+                        angular.extend(newBlade.dataQuery, searchData);
+                    }
+                    else {
+                        var data = {
+                            listEntries: gridApi.selection.getSelectedRows()
+                        };
+
+                        angular.extend(newBlade.dataQuery, data);
+                    }
+                    
+                    bladeNavigationService.showBlade(newBlade);
+
                 },
                 canExecuteMethod: isItemsChecked,
-                index: 20,
+                index: 20
             }, 'virtoCommerce.catalogModule.categoriesItemsListController');
         }
 ]);
