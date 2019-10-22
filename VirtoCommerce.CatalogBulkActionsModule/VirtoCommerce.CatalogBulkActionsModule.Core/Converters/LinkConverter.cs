4b85d31e6ef303808a01152fda9a3beefd22cf23
Timofey <tburashov@gmail.com>
2019-10-22 10:55:54 +0700

    VP-773: add converters

---
+++ VirtoCommerce.CatalogBulkActionsModule/VirtoCommerce.CatalogBulkActionsModule.Core/Converters/LinkConverter.cs
@@ -0,0 +1,29 @@
+﻿namespace VirtoCommerce.CatalogBulkActionsModule.Core.Converters
+{
+    using Omu.ValueInjecter;
+
+    using CategoryLink = VirtoCommerce.CatalogBulkActionsModule.Core.Models.CategoryLink;
+    using VC = VirtoCommerce.Domain.Catalog.Model;
+
+    public static class LinkConverter
+    {
+        public static VC.CategoryLink ToCoreModel(this CategoryLink link)
+        {
+            var result = new VC.CategoryLink();
+            result.InjectFrom(link);
+            return result;
+        }
+
+        public static CategoryLink ToWebModel(this VC.CategoryLink link)
+        {
+            var result = new CategoryLink
+            {
+                CatalogId = link.CatalogId,
+                CategoryId = link.CategoryId,
+                Priority = link.Priority
+            };
+
+            return result;
+        }
+    }
+}
---
+++ VirtoCommerce.CatalogBulkActionsModule/VirtoCommerce.CatalogBulkActionsModule.Core/Converters/SearchCriteriaConverter.cs
@@ -0,0 +1,39 @@
+﻿//namespace VirtoCommerce.CatalogBulkActionsModule.Core.Converters
+//{
+//    using System.Linq;
+
+//    using Omu.ValueInjecter;
+
+//    using VirtoCommerce.BulkActionsModule.Core;
+//    using VirtoCommerce.Platform.Core.Common;
+
+//    using SearchCriteria = VirtoCommerce.CatalogBulkActionsModule.Core.Models.SearchCriteria;
+//    using VC = VirtoCommerce.Domain.Catalog.Model;
+
+//    public static class SearchCriteriaConverter
+//    {
+//        public static VC.SearchCriteria ToCoreModel(this ISearchCriteria criteria)
+//        {
+//            var result = new VC.SearchCriteria();
+
+//            result.InjectFrom(criteria);
+
+//            //result.ResponseGroup = criteria.ResponseGroup;
+//            //result.CategoryIds = criteria.CategoryIds;
+//            //result.CatalogIds = criteria.CatalogIds;
+//            //result.PricelistIds = criteria.PricelistIds;
+//            //result.Terms = criteria.Terms;
+//            //result.Facets = criteria.Facets;
+//            //result.ProductTypes = criteria.ProductTypes;
+//            //result.VendorIds = criteria.VendorIds;
+
+//            //if (!criteria.PropertyValues.IsNullOrEmpty())
+//            //{
+//            //    result.PropertyValues = criteria.PropertyValues.Select(propertyValue => propertyValue.ToCoreModel())
+//            //        .ToArray();
+//            //}
+
+//            return result;
+//        }
+//    }
+//}
