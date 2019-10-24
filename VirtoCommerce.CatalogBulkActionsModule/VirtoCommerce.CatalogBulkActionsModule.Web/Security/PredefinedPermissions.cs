d06467c90612409cfc919e352b8c0c43a6dcb917
Timofey <tburashov@gmail.com>
2019-10-24 16:11:49 +0700

    VP-775: update the module's predefined permissions

---
+++ VirtoCommerce.CatalogBulkActionsModule/VirtoCommerce.CatalogBulkActionsModule.Web/Security/PredefinedPermissions.cs
@@ -0,0 +1,9 @@
+﻿namespace VirtoCommerce.CatalogBulkActionsModule.Web.Security
+{
+    public static class PredefinedPermissions
+    {
+        public const string CategoryChange = "bulk-action:category:change";
+
+        public const string PropertiesUpdate = "bulk-action:properties:update";
+    }
+}
