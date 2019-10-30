39ca5b0215d3dd937a64beace3ff5e270dc21ad5
Timofey <tburashov@gmail.com>
2019-10-30 18:06:11 +0700

    VP-807: add class ctor tests

---
+++ VirtoCommerce.CatalogBulkActionsModule/VirtoCommerce.CatalogBulkActionsModule.Tests/ClassCtorTests.cs
@@ -0,0 +1,67 @@
+﻿namespace VirtoCommerce.CatalogBulkActionsModule.Tests
+{
+    using System;
+
+    using FluentAssertions;
+
+    using Moq;
+
+    using VirtoCommerce.CatalogBulkActionsModule.Core;
+    using VirtoCommerce.CatalogBulkActionsModule.Data.Actions.CategoryChange;
+    using VirtoCommerce.CatalogBulkActionsModule.Data.Actions.PropertiesUpdate;
+    using VirtoCommerce.CatalogBulkActionsModule.Data.DataSources;
+    using VirtoCommerce.CatalogModule.Web.Services;
+
+    using Xunit;
+
+    public class ClassCtorTests
+    {
+        [Fact]
+        public void PropertiesUpdateBulkAction_ShouldThrow_ArgumentException()
+        {
+            // arrange
+
+            // act
+            var action = new Action(
+                () =>
+                {
+                    new PropertiesUpdateBulkAction(Mock.Of<ILazyServiceProvider>(), null);
+                });
+
+            // assert
+            action.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void BaseDataSource_ShouldThrow_ArgumentException()
+        {
+            // arrange
+
+            // act
+            var action = new Action(
+                () =>
+                {
+                    new BaseDataSource(Mock.Of<IListEntrySearchService>(), null);
+                });
+
+            // assert
+            action.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void CategoryChangeBulkAction_ShouldThrow_ArgumentException()
+        {
+            // arrange
+
+            // act
+            var action = new Action(
+                () =>
+                {
+                    new CategoryChangeBulkAction(Mock.Of<ILazyServiceProvider>(), null);
+                });
+
+            // assert
+            action.Should().Throw<ArgumentException>();
+        }
+    }
+}
